69887d064aa6e2210c09c890e7b4f59cd6a5f41b
musekwa <musekwa2011@gmail.com>
2021-08-25 16:50:33 +0200

    cleaning up

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Anchors } from "./Anchors";
+import "./App.css";
+import { Button } from "./Button";
+import { QuoteText } from "./QuoteText";
+
+export const QuoteBox = (props) => {
+  return (
+    <div id="quote-box">
+      <div className="bg-white">
+        <QuoteText bg={props.bg} quote={props.quote} quoter={props.quoter} />
+        <div className="container">
+          <Anchors bg={props.bg} />
+          <div id="buttons">
+            <Button bg={props.bg} callback={props.callback} />
+          </div>
+        </div>
+      </div>
+      <div>
+        <footer id="footer">
+          <p>by Musekwa</p>
+        </footer>
+      </div>
+    </div>
+  );
+};
